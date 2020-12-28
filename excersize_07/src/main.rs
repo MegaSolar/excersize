69a19cdf37a3f1b279f102efcf4e46917bd24140
Megasolar <cmcm3433@gmail.com>
2020-12-28 22:46:49 +0900

    add rustbook course

--- excersize_07/src/main.rs
+++ excersize_07/src/main.rs
@@ -3,12 +3,36 @@
 // 출력문과 계산 부분 분리할것
 // 상수를 사용하여 변환 상수를 저장할것.
 // 제곱피트에서 제곱 미터로 변환하는 공식  :: m*m = f*f* 0.09290304
+use std::io;
+use std::io::Write;
+use std::ops::Mul;
 
 fn main() {
-    println!("what is the length of the room in feet?");
-    println!("what is the width of the room in feet?");
-    println!("Yo0u entered dimensions of XX feet by XX feet");
+    let mut length_str= String::new();
+    let mut witdth_str = String::new();
+    let sq_meter_num = 0.09290304;
+    loop {
+        print!("what is the length of the room in feet?");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut length_str).expect("test");
+        if length_str.trim().is_empty() == true {println!("go to start");}
+
+    }
+
+    print!("what is the width of the room in feet?");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut witdth_str).expect("Can't get width");
+    let res = cal_room(length_str.to_owned(),witdth_str.to_owned());
+    let res_sq = res.mul(res) as f64;
+    let res_meter = res_sq.mul(sq_meter_num);
+    println!("You entered dimensions of {} feet by {} feet .",length_str.trim().to_string(),witdth_str.trim().to_string());
     println!("The area is ");
-    println!("XX square feet ");
-    println!("XXX square meters");
+    println!("{} square feet ",res.to_string());
+    println!("{} square meters",res_meter.to_string());
+}
+fn cal_room ( length: String  , width:String) ->i64 {
+    let length_feet : i64 = length.trim().parse().unwrap();
+    let width_feet : i64 = width.trim().parse().unwrap();
+    return length_feet * width_feet;
+
 }
--- excersize_08/src/main.rs
+++ excersize_08/src/main.rs
@@ -3,13 +3,51 @@
 // 피자 파티
 // 입력값을 숫자로만 받을 수 있도록 수정 >> 숫자가 아니면 프로그램이 진행되지 않음
 // 사람수 , 피자 개수 , 피자조각 개수
+
+use std::io;
+use std::io::Write;
+use std::any::type_name;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use env_logger::Env;
+use env_logger::init;
 fn main() {
-    println!("How many people?");
-    println!("How many pizzas do ytou have");
-    println!("How many pizzas are in a pizza?");
-    println!("8 people with 2 pizzas");
-    println!("Eachg person gets 2 pieces of pizzas");
-    println!("There are 0 leftover pieces");
+    let env = Env::default()
+        .filter_or("MY_LOG_LEVEL", "debug")
+        .write_style_or("MY_LOG_STYLE", "always");
+    env_logger::init_from_env(env);
+
+    let mut people = String::new();
+    let mut pizza = String::new();
+    let mut pieces = String::new();
+    loop{
+        print!("How many people?");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut people).expect("cant get people.");
+        if people.trim().is_empty() {
+            match people.cmp() {
+
+            }
+            continue;
+        }else {
+            println!("How many pizzas do you have?");
+        }
+
+    }
 
 
+
+    // println!("How many pizzas are in a pizza?");
+    // io::stdout().flush().unwrap();
+    // println!("{} people with 2 pizzas");
+    // println!("Each person gets {} pieces of pizzas");
+    // println!("There are {} leftover pieces");
+
+
+}
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
 }
